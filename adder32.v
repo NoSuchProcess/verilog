8f60cb4d74039f73fe8f4c93770243358c4d2774
Сергей Степанов <S.Stepanov@ideco.ru>
2015-06-05 17:14:04 +0500

    add adder32.v

---
+++ adder32.v
@@ -0,0 +1,10 @@
+module adder32(
+    input wire[31:0] a, b,
+    input wire carry_in,
+    output wire[31:0] result,
+    output wire carry_out
+);
+
+assign {carry_out, result} = {1'b0, a} + {1'b0, b} + {32'b0, carry_in};
+
+endmodule
