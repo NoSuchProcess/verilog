565a6e7ba28268d74d4189ea02502c201257ab91
Sergey Stepanov <no.such.process@gmail.com>
2015-06-09 03:31:37 +0500

    fixup adder1

--- adder1.v
+++ adder1.v
@@ -5,12 +5,6 @@ module adder1(
     output wire carry_out
 );
 
-//assign half_carry_ab = a & b;
-//assign half_sum = a ^ b;
-//assign half_carry_in = half_sum & carry_in;
-//assign result = half_sum ^ carry_in;
-//assign carry_out = half_carry_ab | half_carry_in;
-
 assign {carry_out, result} = {1'b0, a} + {1'b0, b} + {1'b0, carry_in};
 
 endmodule
